9dab0071836456d645463f0c09daeaa14c2510b3
tgartland89 <thomas.gartland@gmail.com>
2023-07-11 10:37:47 -0600

    scientsits routes working

--- server/app.py
+++ server/app.py
@@ -20,10 +20,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api =Api(app)
+# don't forget this! to init RESTful 
+
 
 @app.route('/')
 def home():
     return ''
+
+class Scientists(Resource):
+
+    def get(self):
+        scientists = [scientist.to_dict(rules=('-missions', '-planets',))
+                      for scientist in Scientist.query.all()]
+
+        return make_response(scientists, 200)
+
+    def post(self):
+
+        fields = request.get_json()
+        try:
+            scientist = Scientist(
+                name=fields['name'],
+                field_of_study=fields['field_of_study']
+            )
+            db.session.add(scientist)
+            db.session.commit()
+            return make_response(scientist.to_dict(rules=('-missions',)), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+
+api.add_resource(Scientists, "/scientists")
+
+class SceinetistId(Resource):
+    pass 
+
+class Planets(Resource):
+    pass 
+
+class Missions(Resource):
+    pass 
 
 
 if __name__ == '__main__':
